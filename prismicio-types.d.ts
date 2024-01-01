fadf3499b35a10f21ee7ab3d9898b07933977105
Neil Mastroianni <nf_mastroianni@hotmail.com>
2024-01-01 06:37:39 -0500

    feat: added default hero

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,7 +4,7 @@ import type * as prismic from '@prismicio/client'
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] }
 
-type HomepageDocumentDataSlicesSlice = ContentSlice
+type HomepageDocumentDataSlicesSlice = HeroSlice | ContentSlice
 
 /**
  * Content for Homepage documents
