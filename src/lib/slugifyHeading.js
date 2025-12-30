2f2aa2abf85709a06e8b940cf51629688b39e4f2
Neil Mastroianni <nf_mastroianni@hotmail.com>
2025-12-30 10:13:46 -0500

    feat: reboot with fresh Next.js setup

--- src/lib/slugifyHeading.js
+++
@@ -1,7 +0,0 @@
-export const slugifyHeading = ({ text }) => {
-  return text
-    .toLowerCase() // Make lowercase
-    .replace(/[^\w\s-]/g, '') // Remove all non-word chars
-    .replace(/[\s_-]+/g, '-') // Replace spaces and hyphens with a single hyphen
-    .replace(/(^-|-$)/g, '') // Remove leading and trailing hyphens
-}
